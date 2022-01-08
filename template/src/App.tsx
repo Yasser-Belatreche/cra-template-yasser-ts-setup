97945ca46e39ec22743ad2d7901e10ba07bdbd7e
yasserBelatreche <yasser.belatreche0@gmail.com>
2022-01-08 08:22:18 +0100

    add credit at home page + add api file

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import { Pages } from "./pages";
 
-// PLEASE MAKE SURE TO CHECK README FILE BEFORE RUNNING THE APP TO AVOID ERRORS
-
 function App() {
   return <Pages />;
 }
--- template/src/index.tsx
+++ template/src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 
-// PLEASE MAKE SURE TO CHECK README FILE BEFORE RUNNING THE APP TO AVOID ERRORS
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
